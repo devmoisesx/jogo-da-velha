169f243935b8b61a884f5d6e4d8c0e0f7161f254
Moises Xavier <secondmox91@gmail.com>
2024-03-15 14:27:37 -0300

    style: estiliza o header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,11 @@
 import Logo from "../../objects/Logo";
 import Sobre from "../../objects/Sobre";
 import Menu from "../../objects/Menu";
+import "./styles.css";
 
 function Header() {
   return (
-    <header>
+    <header className="header">
       <Logo />
       <Sobre />
       <Menu />
