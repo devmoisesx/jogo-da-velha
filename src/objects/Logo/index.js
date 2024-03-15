473006bc34831df3199b2a8715d15c199513983b
Moises Xavier <secondmox91@gmail.com>
2024-03-15 14:55:30 -0300

    corrige a estrutura do código

--- src/objects/Logo/index.js
+++ src/objects/Logo/index.js
@@ -1,9 +1,11 @@
-import "./styles.css" 
+import "./styles.css";
 
 function Logo() {
-    return (
-        <h1 className="logo">Collab<span>code</span></h1>
-    )
+  return (
+    <h1 className="logo">
+      Collab<span>code</span>
+    </h1>
+  );
 }
 
-export default Logo
+export default Logo;
--- src/objects/Menu/index.js
+++ src/objects/Menu/index.js
@@ -1,7 +1,11 @@
 import "./styles.css";
 
 function Menu() {
-return <a className="menu"><sapn className="center">Menu</sapn></a>;
+  return (
+    <a href="#to-do" className="menu">
+      <sapn className="center">Menu</sapn>
+    </a>
+  );
 }
 
 export default Menu;
