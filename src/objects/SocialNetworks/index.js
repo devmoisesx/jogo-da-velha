2d0c77dfd7a884aef515ec7ecda1f63d1ce3d609
Moises Xavier <secondmox91@gmail.com>
2024-03-26 10:43:10 -0300

    feat: cria o object de links para redes sociais

---
+++ src/objects/SocialNetworks/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./styles.css";
+
+const SocialNetworks = () => {
+  return (
+    <ul className="socialNetworks">
+      <li className="social">
+        <a href="#to-do" className="link">
+          <div className="circle"></div>
+        </a>
+      </li>
+      <li className="social">
+        <a href="#to-do" className="link">
+          <div className="circle"></div>
+        </a>
+      </li>
+      <li className="social">
+        <a href="#to-do" className="link">
+          <div className="circle"></div>
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export default SocialNetworks;
